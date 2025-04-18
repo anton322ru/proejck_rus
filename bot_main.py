3be25360306305e981865028d1fa9a7a2500d8b6
okumuralapis <okumuralapis.makarenko@gmail.com>
2025-04-18 22:58:57 +0400

    бот(!)

--- bot_main.py
+++ bot_main.py
@@ -4,6 +4,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from aiogram.types import ReplyKeyboardRemove, KeyboardButton, ReplyKeyboardMarkup
+import sqlite3
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -12,6 +13,8 @@ with open('config.json') as f:
     con = json.load(f)
 token = con['BOT_TOKEN']
 
+con = sqlite3.connect('db/tasks.db')
+cur = con.cursor()
 reply_keyboard = [[KeyboardButton(text='4 номера', callback_data='4_tasks'),
                    KeyboardButton(text='9 номера', callback_data='9_tasks')],
                   [KeyboardButton(text='10 номера', callback_data='10_tasks'),
@@ -19,10 +22,15 @@ reply_keyboard = [[KeyboardButton(text='4 номера', callback_data='4_tasks'
                   [KeyboardButton(text="Список сложных слов", callback_data='word_list')],
                   [KeyboardButton(text="Добавить слово", callback_data='add_word'),
                    KeyboardButton(text="остановить", callback_data='stop')]]
-kb = ReplyKeyboardMarkup(keyboard=reply_keyboard)
+kb = ReplyKeyboardMarkup(keyboard=reply_keyboard,
+                         input_field_placeholder="Воспользуйтесь меню:")
 
-second_keyboard = [[KeyboardButton(text='Закончить', callback_data='exit_task')]]
-s_kb = ReplyKeyboardMarkup(keyboard=second_keyboard)
+tasks_keyboard = [[KeyboardButton(text='Закончить', callback_data='exit_task')]]
+s_kb = ReplyKeyboardMarkup(keyboard=tasks_keyboard)
+
+permit_keyboard = [[KeyboardButton(text='Закончить', callback_data='exit_task'),
+                    KeyboardButton(text='Продолжить', callback_data='continue_task')]]
+prem_kb = ReplyKeyboardMarkup(keyboard=permit_keyboard)
 dp = Dispatcher()
 
 
@@ -36,6 +44,7 @@ async def main():
 async def process_start_command(message: types.Message):
     await message.reply(f"Привет, {message.from_user.full_name}!\nВыбирай, что будешь делать сегодня",
                         reply_markup=kb)
+    print(message.text)
 
 
 @dp.message(F.text == "остановить")
@@ -50,9 +59,21 @@ async def process_help_command(message: types.Message):
     await message.reply('\n'.join(text))
 
 
+req = cur.execute("""SELECT * FROM '4_task' ORDER BY RANDOM();""").fetchone()
+
+
+# await message.reply("здесь будет викторина по 4", reply_markup=s_kb)
+#     que, right, wrong = req
+#
+#     await message.reply("здесь будет викторина по 4", reply_markup=s_kb)
+
 @dp.message(F.text == "4 номера")
 async def four_task(message: types.Message):
-    await message.reply("здесь будет викторина по 4", reply_markup=s_kb)
+    await message.reply("Вам будет дано слово,\nв котором не указано ударение\nВыберите правильный ответ",
+                        reply_markup=prem_kb)
+
+
+
 
 
 @dp.message(F.text == "9 номера")
