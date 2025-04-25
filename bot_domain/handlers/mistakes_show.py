55a676422f040bbd38b5c5a4252b60003392bb57
okumuralapis <okumuralapis.makarenko@gmail.com>
2025-04-25 16:32:45 +0400

    показ ошибок работает

--- bot_domain/handlers/mistakes_show.py
+++ bot_domain/handlers/mistakes_show.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message
 from bot_domain.keyboards.continue_task import cont_or_exit
 import sqlite3
+from aiogram.enums import ParseMode
 
 router = Router()
 con = sqlite3.connect('../db/users.db')
@@ -13,8 +14,15 @@ cur_task = con_task.cursor()
 
 @router.message(F.text == 'Список ошибок')
 async def show_mist(message: Message):
-    req_4 = cur.execute(f"SELECT mistakes_4 FROM users_tg WHERE id = {message.from_user.id}").fetchone()[0].split(
-        ';')
-    res_4 = cur_task.execute(f"SELECT correct FROM '4_task' WHERE id IN ({', '.join(req_4)})").fetchall()
-    res_4 = [i[0] for i in res_4]
-    await message.answer(f'Ваши ошибки:\n{"\n".join(res_4)}', reply_markup=cont_or_exit('mistakes'))
+    reres = {}
+    for i in (4, 9, 10):
+        req = cur.execute(f"SELECT mistakes_{i} FROM users_tg WHERE id = {message.from_user.id}").fetchone()[0].split(
+            ';')
+        res = cur_task.execute(f"SELECT correct FROM '{i}_task' WHERE id IN ({', '.join(req)})").fetchall()
+        res = [i[0] for i in res]
+        reres[i] = res
+    res_text = ''
+    for num, i in reres.items():
+        res_text += f'\n<b>{num} НОМЕР</b>\n'
+        res_text += '\n'.join(i)
+    await message.answer(f'Ваши ошибки:\n{res_text}', reply_markup=cont_or_exit('mistakes'), parse_mode=ParseMode.HTML)
--- bot_domain/handlers/nine_task.py
+++ bot_domain/handlers/nine_task.py
@@ -13,25 +13,40 @@ que, right, wrong = 0, 0, 0
 @router.message(F.text == 'Продолжить 9 номера')
 @router.message(F.text == '9 номера')
 async def start_nine_task(message: Message):
-    global que, right, wrong
+    global id_req, que, right, wrong
     req = cur.execute("""SELECT * FROM '9_task' ORDER BY RANDOM();""").fetchone()
-    que, right, wrong = req
+    id_req, que, right, wrong = req
     await message.answer(f'Выберите правильное написание слова: {que}', reply_markup=get_cor_or_wrong(right, wrong, 9))
 
 
 @router.callback_query(F.data == 'correct_9')
 async def right_choice(call: CallbackQuery):
-    global que, right, wrong
+    global id_req, que, right, wrong
     await call.message.delete()
     await call.message.answer('Правильно', reply_markup=cont_or_exit(9))
     req = cur.execute("""SELECT * FROM '9_task' ORDER BY RANDOM();""").fetchone()
-    que, right, wrong = req
+    id_req, que, right, wrong = req
+
+
+con_user = sqlite3.connect('../db/users.db')
+cur_user = con_user.cursor()
 
 
 @router.callback_query(F.data == 'wrong_9')
 async def wrong_choice(call: CallbackQuery):
-    global que, right, wrong
+    global id_req, que, right, wrong
     await call.message.delete()
+    req_all_mistakes = \
+        cur_user.execute(f"""SELECT mistakes_9 FROM users_tg WHERE id = {call.from_user.id}""").fetchone()[0]
+    if req_all_mistakes is not None:
+        res_mistakes = f"{req_all_mistakes};{id_req}"
+    else:
+        res_mistakes = f'{id_req}'
+    req = cur_user.execute(f"""UPDATE users_tg 
+                                 SET mistakes_9 = '{res_mistakes}'
+                                 WHERE id = {call.from_user.id};""")
+    con_user.commit()
+
     await call.message.answer(f'Неправильно. Правильное написание: {right}', reply_markup=cont_or_exit(9))
     req = cur.execute("""SELECT * FROM '9_task' ORDER BY RANDOM();""").fetchone()
-    que, right, wrong = req
+    id_req, que, right, wrong = req
